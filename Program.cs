75d411ee7d6f59765a37e1a64ec0f519bf8dacc1
Muhammed Kutukcuoglu <kutukcuoglu007@gmail.com>
2024-03-07 23:11:00 +0300

    C# first project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ad: ");
+var ad = Console.ReadLine();
+
+Console.Write("soyad: ");
+var soyad = Console.ReadLine();
+
+Console.Write("yas: ");
+var yas = Console.ReadLine();
+
+string mesaj = ad + " " + soyad + " " + "arkadasım" + " " + yas + " " + "yasında" ;
+
+Console.WriteLine(mesaj);
+
